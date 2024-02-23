4619ffb471ea5e73a276e6c705ee13f4ac5aa9d4
Soubhagya <58697292+SoubhagyaSDas@users.noreply.github.com>
2024-02-23 01:25:00 -0500

    palindrome commit

--- Recursion/13_Palindrome.cpp
+++ Recursion/13_Palindrome.cpp
@@ -1,21 +1,27 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-string check(string str, int size) {
-    if(size == 0) {
-        return " ";
+string rev_str(const string& str) {
+    if(str.length() <= 1) {
+        return str;
     }
-    string rev_str = check(str, size - 1);
-    return rev_str;
+
+    string reverse = rev_str(str.substr(0, str.length() - 1));
+    return str[str.length() - 1] + reverse;
 }
 
 int main() {
-    string str;
+    string str1, str2;
     cout << "Enter the string: ";
-    cin >> str;
-    string str1 = check(str, str.length());
-    cout << str1 << endl;
-    
+    cin >> str1;
+
+    str2 = rev_str(str1);
+    if(str2 == str1) {
+        cout << "Yes, the string is a palindrome." << endl;
+    } else {
+        cout << "No, the string is a palindrome." << endl;
+    }
+    return 0;
 }
