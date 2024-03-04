765ab8bc0bf1230e6c00848ffd95a9a46150a915
Soubhagya <58697292+SoubhagyaSDas@users.noreply.github.com>
2024-03-04 18:41:08 -0500

    subsequences

--- Recursion/Strings and Recursion/11_Subsequences.cpp
+++ Recursion/Strings and Recursion/11_Subsequences.cpp
@@ -11,8 +11,18 @@ int subs(string& input, string output[]) {
         return 1;
     }
 
-    // 
+    // substring to return all the substrings, each time removing the first character from the string
+    string smallString = input.substr(1);
 
+    // recursive call over the subsequences function
+    int smallOutputSize = subs(smallString, output);
+
+    // adds the first character into all the subsequences
+    for (int i = 0; i < smallOutputSize; i++) {
+        output[i + smallOutputSize] = input[0] + output[i];
+    }
+
+    return 2 * smallOutputSize;
 }
 
 int main() {
